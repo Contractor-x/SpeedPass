a443f84cc9dd61820c2cda538ab1a22a50a13239
Collins Dada <140630132+Contractor-x@users.noreply.github.com>
2025-06-25 00:12:34 +0100

    Gmail alert System

--- app/alert_system.py
+++ app/alert_system.py
@@ -1,1 +1,20 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+def send_email(to, subject, body):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = EMAIL
+    msg["To"] = to
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL, PASSWORD)
+        server.send_message(msg)
 
